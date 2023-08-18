4096cac8ff58d6f12c12e70e05e015e34c07d9d4
nionroy <136985474+nionroy@users.noreply.github.com>
2023-08-18 19:50:36 +0600

    Add files via upload

---
+++ arcitecture.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+#include<windows.h>
+using namespace std;
+int main(){
+cout<<"Nion"<<endl;
+Sleep(2000);
+cout<<"ROY"<<endl;
+cout<<"enter the numbere"<<endl;
+int t;
+cin>>t;
+if(t<12){
+    system("cls");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+return 0;
+}
